1bd6dc319d9f5543ca1c05973b47b7b6df9f2f03
Luis Manuel González <mannyglzg@gmail.com>
2020-11-19 21:19:00 -0800

    Add html to about, sign in form and sign up form
    Add Angular, MongoDB and NestJS images to assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { RouterModule } from '@angular/router';
 import { HomeModule } from './modules/home/home.module';
 import { NotesModule } from './modules/notes/notes.module';
 
