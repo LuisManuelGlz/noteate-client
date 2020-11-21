3fee671a44e83cf272dd6067cf6e7359ad4ab9f7
Luis Manuel González <mannyglzg@gmail.com>
2020-11-21 14:39:35 -0800

    Add authentication interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './modules/home/home.module';
 import { NotesModule } from './modules/notes/notes.module';
+import { AuthenticationInterceptor } from './core/interceptors/authentication.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +18,13 @@ import { NotesModule } from './modules/notes/notes.module';
     HomeModule,
     NotesModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthenticationInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
