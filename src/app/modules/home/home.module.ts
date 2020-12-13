cd7a85068a18feeeeb620c3e31ecd34e2eddba90
Luis Manuel González <mannyglzg@gmail.com>
2020-12-12 17:22:28 -0800

    Add sign in page

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -8,6 +8,7 @@ import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 import { SignInFormComponent } from './components/sign-in-form/sign-in-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     SignUpComponent,
     SignUpFormComponent,
     SignInFormComponent,
+    SignInComponent,
   ],
   imports: [CommonModule, HomeRoutingModule, ReactiveFormsModule],
 })
--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +8,12 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
+  }
 }
